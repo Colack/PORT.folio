293d14453a81272006fd3d6567e65baaf5d53ed9
Jack Spencer <UniformKitten281@gmail.com>
2024-05-10 10:14:05 -0700

    Basic C++ code

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+
+#include <SFML/Window.h>
+#include <SFML/Graphics.h>
+
+// This is my portfolio project, a simple thing that links to all my other projects.
